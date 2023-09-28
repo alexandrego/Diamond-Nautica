f3516215e5e446c92d9ea3f3edc07955c3dcbcfd
Alexandre Goncalves <desenvolvimento@diamondnautica.com.br>
2023-09-28 17:58:54 -0300

    chore: Desenvolvendo...👨🏾‍💻

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,17 +6,19 @@
         <input type="text" class="form-search" placeholder="Pesquise aqui" />
     </div>
     <div>
-        <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"><i class="fa-solid fa-bars"></i></button>
+        <button class="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"><i class="fa-solid fa-bars btn-icons"></i></button>
 
         <div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
-        <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Offcanvas with body scrolling</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div class="offcanvas-body">
-            <p>Try scrolling the rest of the page to see this option in action.</p>
-        </div>
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Offcanvas with body scrolling</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <p>Try scrolling the rest of the page to see this option in action.</p>
+            </div>
         </div>
     </div>
+    <div>
+        <i class="fa-solid fa-cart-shopping btn-icons"></i>
     </div>
 </div>
