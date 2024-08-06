8361cb142b47a24b1dbaa8b4aca67076d918ff43
Alexandre Goncalves <desenvolvimento@diamondnautica.com.br>
2024-08-06 16:29:39 -0300

    chore: Ajustando Efeito Skeleton

--- public/src/js/scripts.js
+++ public/src/js/scripts.js
@@ -64,15 +64,22 @@ function handleProductClick() {
         behavior: 'smooth'
     });
 
+    // Verifica se é uma navegação de volta
+    if (isBack) {
+        // Atualiza o histórico do navegador
+        // window.history.replaceState({}, '', url);
+        location.reload(); // Recarrega a página
+    }
+
     // Simular um atraso antes de voltar (ajuste o tempo conforme necessário)
-    setTimeout(function() {
-        // Remover o efeito skeleton
-        const skeleton = document.getElementById('skeleton');
-        if (skeleton) {
-            skeleton.style.display = 'none'; // Oculta o skeleton
-        }
-        // location.reload(); // Recarrega a página
-    }, 4000); // 1000 ms = 1 segundo
+    // setTimeout(function() {
+    //     // Remover o efeito skeleton
+    //     const skeleton = document.getElementById('skeleton');
+    //     if (skeleton) {
+    //         skeleton.style.display = 'none'; // Oculta o skeleton
+    //     }
+    //     location.reload(); // Recarrega a página
+    // }, 4000); // 1000 ms = 1 segundo
 }
 
 // Voltando da página de produtos
