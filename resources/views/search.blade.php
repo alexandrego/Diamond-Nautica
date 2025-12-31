7c5bef0fa54ba46bcd2a328d83b80b99aed0b04f
Alexandre Goncalves <desenvolvimento@diamondnautica.com.br>
2025-12-31 14:27:20 -0300

    Ajustando input Search

---
+++ resources/views/search.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.main')
+
+@section('content')
+<div class="search-page">
+    <div class="search-header">
+        <h1>Resultados da Busca</h1>
+        <p>Buscando por: "{{ $query }}"</p>
+    </div>
+    <div id="search-results-container" class="search-results-container">
+        <!-- Search results will be loaded here via JavaScript -->
+    </div>
+</div>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        loadSearchResults('{{ $query }}');
+    });
+</script>
+@endsection
