ef1281bd012a6ef3706586655ccd8027f9c3a260
Alexandre Gonçalves <alexandre_c_g@hotmail.com>
2025-12-31 11:04:43 -0300

    chore: Splash Screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,13 +20,19 @@
     }
     const splashscreen = setTimeout(bemVindo, 800);
 
+    let progressInterval;
+    let fetchDone = false;
+
     // Start progress bar after welcome message
     setTimeout(function() {
         const progressBar = document.getElementById('progressBar');
         let width = 0;
-        const interval = setInterval(function() {
+        progressInterval = setInterval(function() {
             if (width >= 100) {
-                clearInterval(interval);
+                clearInterval(progressInterval);
+                if (fetchDone) {
+                    window.location.href = "/home";
+                }
             } else {
                 width++;
                 progressBar.style.width = width + '%';
@@ -34,7 +40,20 @@
         }, 32); // 3200ms / 100 = 32ms per percent
     }, 800);
 
-    const splash = setTimeout(function() {
-        window.location.href = "/home";
-    }, 4000);
+    // Fetch products to ensure home is ready
+    fetch('https://diamondnautica.com.br/wp-json/diamondnautica/v2/products?page=1&per_page=10')
+        .then(response => response.json())
+        .then(data => {
+            fetchDone = true;
+            if (document.getElementById('progressBar').style.width === '100%') {
+                window.location.href = "/home";
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching products:', error);
+            fetchDone = true;
+            if (document.getElementById('progressBar').style.width === '100%') {
+                window.location.href = "/home";
+            }
+        });
 </script>
